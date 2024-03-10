f814aa710fcccb138eabc814024e4b9f1c1872bf
rgdnk <ruslan.godynyuk@gmail.com>
2024-03-10 23:50:56 +0300

    make corrections after CR

--- postgres_to_es/config/entrypoint.sh
+++ postgres_to_es/config/entrypoint.sh
@@ -2,14 +2,20 @@
 
 readonly cmd="$*"
 
-while ! nc -z elastic 9200; do
+export SCHEMA=${ELASTICSEARCH_SCHEMA}
+export HOST=${ELASTICSEARCH_HOST}
+export PORT=${ELASTICSEARCH_PORT}
+
+# Я не пользуюсь этим скриптом, отправляю curl со схемой в отдельном контейнере docker compose
+# Поправил, как смог, обещаю подучить bash в будущем!
+while ! nc -z ${HOST} ${PORT}; do
       sleep 20
       echo "Elasticsearch not ready"
 done
 
 echo "Elasticsearch is ready"
 
-curl -XPUT http://elastic:9200/movies -H 'Content-Type: application/json' -d @/movies.json
+curl -XPUT ${SCHEMA}://${HOST}:${PORT}/movies -H 'Content-Type: application/json' -d @/movies.json
 
 echo "Elasticsearch movie index was created"
 
